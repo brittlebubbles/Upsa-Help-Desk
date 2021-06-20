63eda3b6e1516b95a7d2faefb8e44569f266f364
brittlebubbles <adom.466@gmail.com>
2021-06-20 12:22:55 +0000

    adds purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-  purge: [
-    "./src/**/*.html",
-    "./src/**/*.vue",
-    "./src/**/*.jsx",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./helpers/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layout/**/*.{js,ts,jsx,tsx}",
-    "./helpers/**/*.{js,ts,jsx,tsx}",
-    "./index.html",
-  ],
+  purge: {
+    enable: true,
+    content: [
+      "./public/**/*.html",
+      "./src/**/*.html",
+      "./src/**/*.vue",
+      "./src/**/*.jsx",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+      "./helpers/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./layout/**/*.{js,ts,jsx,tsx}",
+      "./helpers/**/*.{js,ts,jsx,tsx}",
+      "./index.html",
+    ],
+  },
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
